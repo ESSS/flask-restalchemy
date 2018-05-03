b04d3f29ef77d2de24ef95b533ab5fd387345445
Lucas Oliveira <lucaslogradouro@gmail.com>
2018-05-03 08:37:32 -0300

    bumped version to 0.8.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires=[
 
 setup(
     name='flask-restalchemy',
-    version='0.8.1',
+    version='0.8.2',
     packages=find_packages(exclude=['*.tests']),
     url='https://github.com/ESSS/flask-restalchemy',
     license='MIT',
