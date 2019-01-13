b0c329c3a0c22ffee34657b23be61f229688f7a6
Igor T. Ghisi <igor.ghisi@gmail.com>
2019-01-12 22:07:01 -0200

    Add tox config file and use it on Travis CI

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup, find_packages
 install_requires=[
     'flask-restful>=0.3.0',
     'flask-sqlalchemy >= 2.3.0',
-    'flask',
+    'sqlalchemy_utils>=0.30',
+    'flask>=1.0.0',
     'sqlalchemy>=1.1',
 ]
+
+extras_require = {
+    "docs": ["sphinx >= 1.4", "sphinx_rtd_theme", "sphinx-autodoc-typehints"],
+    "testing": ["codecov", "pytest", "pytest-cov", "pytest-mock", "pytest-datadir", "tox"],
+}
 
 setup(
     name='flask-restalchemy',
@@ -14,18 +20,19 @@ setup(
     url='https://github.com/ESSS/flask-restalchemy',
     license='MIT',
     author='ESSS',
-    author_email='dev@esss.co',
+    author_email='foss@esss.co',
     description='Flask extension to build REST APIs based on SQLAlchemy models ',
     keywords='flask sqlalchemy orm',
     data_files=[("", ["LICENSE"])],
     install_requires=install_requires,
-    python_requires='>=3.4',
+    extras_require=extras_require,
+    python_requires='>=3.5',
     classifiers=[
+        'Environment :: Web Environment',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Database :: Front-Ends',
